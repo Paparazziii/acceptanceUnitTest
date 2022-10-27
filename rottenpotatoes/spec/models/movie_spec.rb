16478f32154f4ff68fd3a1da1bb8819405420374
Paparazziii <finiantang0826@gmail.com>
2022-10-27 00:00:09 -0400

    update small typo

--- rottenpotatoes/spec/models/movie_spec.rb
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 describe Movie do
   describe '.find_similar_movies' do
 
-    #movie1 = Movie.new
-    #movie2 = Movie.new
-    #movie3 = Movie.new
-    #movie1.title = "movie1"
-    #movie1.director = "dir1"
-    #movie2.title = "movie2"
-    #movie2.director = "dir1"
-    #movie3.title = "movie3"
     movie1 = Movie.create(:title=>"movie1")
     movie2 = Movie.create(:title=>'movie2',:director=>'dir1')
     movie3 = Movie.create(:title=>'movie3',:director=>'dir1')
